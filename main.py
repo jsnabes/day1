f86e6ccf74512cc332018cd4d46ebe36841f7389
Julia Snabes <julia.snabes@gmail.com>
2023-10-02 09:07:16 -0500

    My first commit - exploring data types and f-string

---
+++ main.py
@@ -0,0 +1,20 @@
+# Screen output
+first_name: str = "Julia"
+print(first_name)
+
+# Initializing variables
+my_first_int: int = 3
+print(my_first_int)
+
+# Exploring backslash
+# Screen output
+first_name: str = "Fred \\ \n"
+print(first_name)
+
+# Exploring f-string
+print(f'The first integer is: {my_first_int}')
+
+# Getting input from the user
+prompt: str = 'What is your name? '
+resp = input(prompt)
+print(resp)
